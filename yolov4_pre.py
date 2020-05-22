7bf22ff3306aa253caf52453d7a326351e360891
ald2004 <yuanpu@139.com>
2020-05-22 19:29:44 +0800

    20200522 add conver to yolov4 format

---
+++ yolov4_pre.py
@@ -0,0 +1,82 @@
+import xml.etree.ElementTree as ET
+import os
+import json
+import glob
+from fvcore.common.file_io import PathManager
+
+
+def convert_xminymin_xcenterycenter(h, w, xmin, ymin, xmax, ymax):
+    # < x_center > < y_center > < width > < height > - float values relative to width and height of image, it can  be  equal from (0.0 to 1.0]
+    dw = 1. / (h)
+    dh = 1. / (w)
+    x = (xmin + ymin) / 2.0
+    y = (xmax + ymax) / 2.0
+    w = ymin - xmin
+    h = ymax - xmax
+    x = round(x * dw, 6)
+    w = round(w * dw, 6)
+    y = round(y * dh, 6)
+    h = round(h * dh, 6)
+    return x, y, w, h
+    # return f'{x} {w} {y} {h}'
+
+
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+
+things_class_dict = {
+    'face-head': 0,
+    'mask-head': 1,
+    'face-cap': 2,
+    'mask-cap': 3
+}
+
+
+def main():
+    xmlfiles = glob.glob("*.xml")
+    for xmlfile in xmlfiles:
+        with PathManager.open(xmlfile) as fid:
+            tree = ET.parse(fid)
+            # < name > mask - cap < / name >
+            # < filename > mq28591952000113 - 065435.jpg < / filename >
+            filename_ = tree.find("filename").text.strip()
+            width_ = tree.find("size").find("width").text.strip()
+            height_ = tree.find("size").find("height").text.strip()
+            depth_ = tree.find("size").find("depth").text.strip()
+            for obj in tree.findall("object"):
+                oclass_ = things_class_dict[tree.find("name").text.strip()]
+                bbox = obj.find("bndbox")
+                try:
+                    bbox: str = convert_xminymin_xcenterycenter(
+                        float(bbox.find("xmin").text),
+                        float(bbox.find("ymin").text),
+                        float(bbox.find("xmax").text),
+                        float(bbox.find("ymax").text)
+                    )
+                except:
+                    pass
+
+
+def unitest():
+    '''
+    <xmin>206.42315644383186</xmin>\n',
+    <ymin>427.1415441176471</ymin>\n',
+    <xmax>288.4631288766368</xmax>\n',
+    <ymax>514.4944852941177</ymax>\n',
+    h 720
+    w 1280
+    '''
+    x, y, w, h = convert_xminymin_xcenterycenter(720, 1280, 206.423, 427.141, 288.463, 514.494)
+    print(x, y, w, h)
+    xmin, ymin, xmax, ymax = convertBack(x, y, w, h)
+    print(xmin, ymin, xmax, ymax)
+
+
+if __name__ == '__main__':
+    unitest()
+
