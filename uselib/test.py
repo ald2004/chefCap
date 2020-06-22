a379e04f07f5c15940c957e25320756c195a2232
ald2004 <yuanpu@139.com>
2020-06-22 16:54:07 +0800

    nothing

--- uselib/test.py
+++ uselib/test.py
@@ -5,7 +5,7 @@ from ctypes import (
     c_void_p, POINTER, c_char_p, pointer,
     c_int, c_float,
 )
-import cv2
+import numpy as np
 
 
 class METADATA(Structure):
@@ -13,20 +13,79 @@ class METADATA(Structure):
                 ("names", POINTER(c_char_p))]
 
 
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+
+class DETECTION(Structure):
+    _fields_ = [("bbox", BOX),
+                ("classes", c_int),
+                ("prob", POINTER(c_float)),
+                ("mask", POINTER(c_float)),
+                ("objectness", c_float),
+                ("sort_class", c_int),
+                ("uc", POINTER(c_float)),
+                ("points", c_int)]
+
+
+class ddx(Structure):
+    _fields_ = [("nboxes", c_int),
+                ("dd", POINTER(DETECTION))]
+
+
+thing_classes = ['face-head', 'mask-head', 'face-cap', 'mask-cap', 'uniform', 'non-uniform']
 lib = CDLL('libuse.so', RTLD_GLOBAL)
 unitest = lib.unitest
-unitest.argtypes = [c_char_p, c_char_p, c_int, c_int, c_char_p, c_char_p,c_int]
-unitest.restype = c_void_p
+unitest.argtypes = [c_char_p, c_char_p, c_char_p, c_int]
+unitest.restype = ddx
 
-load_meta = lib.get_metadata
-lib.get_metadata.argtypes = [c_char_p]
-lib.get_metadata.restype = METADATA
+detect_narray = lib.detect_by_narray
+detect_narray.argtypes = [c_char_p, c_char_p, c_char_p, c_int]
+detect_narray.restype = ddx
 
-im = cv2.imread("41555ee52bab40169b10253ab30259fa_nondetected.jpg")
-frame_rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-# frame_resized = cv2.resize(frame_rgb, (576, 576), interpolation=cv2.INTER_LINEAR)
-unitest("../cfg/chefCap-sam-mish.cfg".encode("ascii"), "../backup/chefCap-sam-mish_last.weights".encode("ascii"), 0, 1,
-        "../cfg/chefCap.data".encode("ascII"), "41555ee52bab40169b10253ab30259fa_nondetected.jpg".encode("ascII"),0)
-# metaMain = load_meta("../cfg/chefCap.names".encode("ascii"))
-# print(metaMain._fields_[0][1])
-# [('classes', <class 'ctypes.c_int'>), ('names', <class '__main__.LP_c_char_p'>)]
+
+def detect(cfg_name="cfg/chefCap-sam-mish.cfg", weights_name="backup/chefCap-sam-mish_last.weights",
+           img_name="test.jpg", gpu_id=0):
+    c_name = "../cfg/chefCap-sam-mish.cfg"  # cfg_name
+    w_name = "../backup/chefCap-sam-mish_last.weights"  # weights_name
+    i_name = "41555ee52bab40169b10253ab30259fa_nondetected.jpg"  # img_name
+    ddx = unitest(c_name.encode("ascii"), w_name.encode("ascii"), i_name.encode("ascII"), 0)
+    dets = ddx.dd
+    res = []
+    for j in range(ddx.nboxes):
+        for i in range(6):
+            if dets[j].prob[i] > 0:
+                b = dets[j].bbox
+                nameTag = thing_classes[i]
+                res.append((nameTag, dets[j].prob[i], (b.x, b.y, b.w, b.h)))
+    return sorted(res, key=lambda x: -x[1])
+
+
+def detect_by_narray(cfg_name="cfg/chefCap-sam-mish.cfg", weights_name="backup/chefCap-sam-mish_last.weights",
+                     img=np.zeros([30, 30, 3], dtype=np.int), gpu_id=0):
+    c_name = "../cfg/chefCap-sam-mish.cfg"  # cfg_name
+    w_name = "../backup/chefCap-sam-mish_last.weights"  # weights_name
+    i_name = img
+    ddx = detect_narray(c_name.encode("ascii"), w_name.encode("ascii"), i_name.tobytes(), 0)
+    dets = ddx.dd
+    res = []
+    for j in range(ddx.nboxes):
+        for i in range(6):
+            if dets[j].prob[i] > 0:
+                b = dets[j].bbox
+                nameTag = thing_classes[i]
+                res.append((nameTag, dets[j].prob[i], (b.x, b.y, b.w, b.h)))
+    return sorted(res, key=lambda x: -x[1])
+
+
+import cv2
+
+size = (576, 576)
+image_src = cv2.imread("41555ee52bab40169b10253ab30259fa_nondetected.jpg")
+image_src = cv2.cvtColor(image_src, cv2.COLOR_BGR2RGB)
+# image_src = cv2.resize(image_src, size, interpolation=cv2.INTER_LINEAR)
+print(detect_by_narray(img=image_src, gpu_id=1))
+
